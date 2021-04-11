eab97368c00ce5b1c6ba06f350fef2e41bdcce31
Fahim <55667286+fahim2k29@users.noreply.github.com>
2021-04-12 02:22:05 +0600

    update design

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,40 @@
 import React from 'react';
+import Badge from '@material-ui/core/Badge';
+import { withStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+const StyledBadge = withStyles((theme) => ({
+    badge: {
+      right: -3,
+      top: 13,
+      border: `2px solid ${theme.palette.background.paper}`,
+      padding: '0 4px',
+    },
+  }))(Badge);
 const Cart = (props) => {
     const cart = props.cart;
-    const cartStyle =  {border:'2px solid red', marginLeft:'70%', padding:'10px', textAlign:'left'};
-    const totalAmount = cart.reduce( (sum, course) => sum + course.price ,0);
+    const cartStyle = { border: '2px solid red', marginLeft: '70%', padding: '10px', textAlign: 'left' };
+    const totalAmount = cart.reduce((sum, course) => sum + course.price, 0);
+    const cartColor = cart.length ? "secondary" : "";
+    let smileFace;
+    if(cart.length > 0 ){
+        smileFace = <InsertEmoticonIcon /> ;
+    } 
+
     return (
         <div style={cartStyle}>
-           <h4>Added Course: {cart.length}</h4>
-           <p>Total Amount: {totalAmount} TK</p>
+            <h4>Added Course:
+                <IconButton aria-label="cart"  color={cartColor}>
+                    <StyledBadge badgeContent={cart.length}  color="primary">
+                        <ShoppingCartIcon /> 
+                    </StyledBadge>
+                </IconButton>
+                    <span> </span> 
+                    <span> </span>    {smileFace}
+
+                </h4>
+            <p>Total Amount: {totalAmount} TK</p>
         </div>
     );
 };
--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -1,29 +1,71 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 450,
+        float:"left",
+        width:"30%", 
+        margin:"8px",
+        border: "2px solid gray"    
+    },
+    media: {
+        height: 100,
+    },
+});
 
 const Course = (props) => {
-   const {name, instructor, duration, price,id} = props.course;
-   const courseStyle =  {border:'2px solid blue', margin:'10px', width:'60%', padding:'10px'};
-   const handleAddCourse = props.handleAddCourse;
-   
-   const history = useHistory();
-   const handleClick = (courseId) => {
-       const url = `/course/${courseId}`;
-       history.push(url);
-   }  
-   
-   
-   return (
-        <div style={courseStyle}>
-            <h4>{name}</h4>
-            <p><b>Course Duration:</b> {duration}</p>
-            <p><b>Instructor:</b> {instructor}</p>
-            <p><b>Price:</b> {price}TK</p>
-            <button onClick={() => handleClick(id)} className="btn btn-info" style={{margin: "20px"}}>Show Details {id}</button>
-            <button onClick={ () => handleAddCourse(props.course)} className="btn btn-primary">Add Course</button>
+    const classes = useStyles();
+    const { name, instructor, duration, price, id, picture } = props.course;
+    const courseStyle = { border: '2px solid blue', margin: '10px', width: '60%', padding: '10px' };
+    const handleAddCourse = props.handleAddCourse;
+    const history = useHistory();
+    const handleClick = (courseId) => {
+        const url = `/course/${courseId}`;
+        history.push(url);
+    }
+
+    return (
+        // <div style={courseStyle}>
+        //     <h4>{name}</h4>
+        //     <p><b>Course Duration:</b> {duration}</p>
+        //     <p><b>Instructor:</b> {instructor}</p>
+        //     <p><b>Price:</b> {price}TK</p>
+        //     <button onClick={() => handleClick(id)} className="btn btn-info" style={{margin: "20px"}}>Show Details {id}</button>
+        //     <button onClick={ () => handleAddCourse(props.course)} className="btn btn-primary">Add Course</button>
+        // </div>
+        <div>
+            <Card className={classes.root}>
+                <CardActionArea>
+                    <CardMedia
+                        className={classes.media}
+                        image={picture}
+                        title="Contemplative Reptile"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            {name}
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                        </Typography>
+                        <p>Instructor: {instructor}</p>
+                        <p>Duration: {duration}</p>
+                        <p>Price: {price} TK</p>
+                    </CardContent>
+                    <button onClick={() => handleClick(id)} className="btn btn-info" style={{ margin: "20px" }}>Show Details</button>
+                    <button onClick={() => handleAddCourse(props.course)} className="btn btn-primary">Add Course</button>
+                </CardActionArea>
+            </Card>
         </div>
-       
     );
 };
 
